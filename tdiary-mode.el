997201beed64c11f46ef98c945001105d790464d
Youhei SASAKI <uwabami@gfd-dennou.org>
2019-01-21 09:18:39 +0900

    Update License, Add README.org
    Signed-off-by: Youhei SASAKI <uwabami@gfd-dennou.org>

--- tdiary-mode.el
+++ tdiary-mode.el
@@ -6,7 +6,7 @@
 ;;         Youhei SASAKI <uwabami@gfd-dennou.org>
 ;; Version: 0.0.2
 ;; Keywords: net
-;; License: GPL-3.0
+;; License: GPL-2.0
 ;;
 ;; This program is free software; you can redistribute it and/or
 ;; modify it under the terms of the GNU General Public License as
